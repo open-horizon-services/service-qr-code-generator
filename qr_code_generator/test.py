3e5b6f41544bea24058f547f87b4d9c2126c420c
WilliamLi10 <william3ny@gmail.com>
2022-11-17 20:35:00 -0500

    made test code for flask web server
    Signed-off-by: WilliamLi10 <william3ny@gmail.com>

---
+++ qr_code_generator/test.py
@@ -0,0 +1,11 @@
+import requests
+from cairosvg import svg2png
+
+BASE = "http://127.0.0.1:5000/"
+
+
+requests.post(BASE + "QR_Code/hello")
+response = requests.get(BASE+ "QR_Code/hello")
+response = response.text
+#result = response["image"]
+print(response)
