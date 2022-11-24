2c7c28fed42ac7b28a04d69d57361f849f42164c
WilliamLi10 <william3ny@gmail.com>
2022-11-23 21:00:19 -0500

    added example code for converting svg to png file in example.py
    Signed-off-by: WilliamLi10 <william3ny@gmail.com>

--- qr_code_generator/example.py
+++ qr_code_generator/example.py
@@ -1,4 +1,5 @@
 import requests
+from json import loads
 from cairosvg import svg2png
 
 BASE = "http://127.0.0.1:5000/"
@@ -7,5 +8,5 @@ BASE = "http://127.0.0.1:5000/"
 requests.post(BASE + "QR_Code/hello")
 response = requests.get(BASE+ "QR_Code/hello")
 response = response.text
-#result = response["image"]
-print(response)
+
+svg2png(bytestring=loads(response)["image"],write_to='output.png')
